3d0d320092698f8df3db23875ac4e1e3c4c5fe8c
DESKTOP-SN22LDF\asa <keccack5@gmail.com>
2017-10-05 15:39:25 +0900

    Practice01_02～07　完了

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -9,12 +9,13 @@ package practice01;
 public class PTra01_02 {
 	public static void main(String[] args) {
 
-		// ★ 12～14行目をコメントにしてください。
+		/* ★ 12～14行目をコメントにしてください。
 		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+		ここをコメントにしてください 
+		ここをコメントにしてください  */
+		System.out.println("Hello, world");
 
-		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
+		 // ★「Hello, world」と出力（コマンドプロンプトに表示）してください
 
 
 	}
