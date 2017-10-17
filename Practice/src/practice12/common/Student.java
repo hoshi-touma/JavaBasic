b17a16671821f48b7b963c26cd92294afa8ae446
DESKTOP-SN22LDF\asa <keccack5@gmail.com>
2017-10-17 15:25:31 +0900

    Practice12　完了

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -13,20 +13,73 @@ package practice12.common;
  */
 public class Student {
 	/** 受講生ID */
-	public int studentId;
+	private int studentId;
 
 	/** 受講生氏名 */
-	public String studentName;
+	private String studentName;	
 
 	/** 会社名 */
-	public String companyName;
-
-	/** 教室名 */
-	public String className;
+	private String companyName;
+	
+    /** 教室名 */
+	private String className;
 
 	/** メールアドレス */
-	public String mail;
+	private String mail;
 
-	/** パスワード */
-	public String password;
+/** パスワード */
+	private String password;
+
+		
+	// 受講生IDのアクセサ
+		public int getStudentId() {
+			return studentId;
+		}
+		public void setStudentid(int num) {
+			this.studentId=num;
+	    }
+		
+	// 氏名のアクセサ
+		public String getStudentName() {
+			return studentName;
+		}
+		public void setStudentName(String name) {
+			this.studentName=name;
+		}
+
+		
+	// 会社名のアクセサ
+		public String getCompanyName() {
+			return companyName;
+		}
+		public void setCompanyName(String ca_name) {
+			this.companyName=ca_name;
+		}
+		
+		
+	// 教室名のアクセサ
+		public String getClassName() {
+			return className;
+		}
+		public void setClassName(String cl_name) {
+			this.className=cl_name;
+		}
+		
+	
+	//メールアドレスのアクセサ
+		public String getMail() {
+			return mail;
+		}
+		public void setMail(String mail) {
+			this.mail=mail;
+		}
+		
+		
+	// パスワードのアクセサ
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String pass) {
+			this.password=pass;
+		}
 }
